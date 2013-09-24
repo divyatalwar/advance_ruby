3a3a918907558b7d04e4afe1f1bf63bb068bc64a
DivyaTalwar <divya@vinsol.com>
2013-09-24 17:34:22 +0530

    review

--- dynamic_class_craetion/lib/csvParser.rb
+++ dynamic_class_craetion/lib/csvParser.rb
@@ -1,14 +1,17 @@
 require "csv"
 class CsvParser
   attr_accessor :file_name , :headers , :rows
+
   def initialize(filename, headers = [], rows = [])
     @file_name = filename
 	  @headers = []
     @rows = []
   end
+
   def filePath
 	  "#{ File.dirname(__FILE__) }/#{ file_name }"
 	end
+  
   def create_custom_class
     temp_array = []
     correct_format = headers.collect { |var| "i"+ var.gsub(" ","_")}
@@ -28,12 +31,14 @@ class CsvParser
     end
     Object.const_set className , new_class
   end
+  
   def create_custom_class_obj(data)
     obj = Object.const_get(className).new
     data.each do | key, value |
       obj.send("#{ @key_hash[key] }=", value)
     end
   end
+  
   def parseCsv
     row = [] 
     options = { :headers => true, :return_headers => false }
@@ -46,12 +51,15 @@ class CsvParser
       rows.push create_custom_class_obj(r)
     end
   end
+  
   def className
     file_name.split('.')[0].chop.capitalize
   end
+  
   def display
     class_name = Object.const_get(className)
     ObjectSpace.each_object( class_name ).collect { | object | p object,object.show_data}
   end 
+
 end
 
